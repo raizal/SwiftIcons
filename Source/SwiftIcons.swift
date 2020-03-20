edd0bd423b496c2803d9275633fedaa5eb4a96ad
Raizal I.N. Pregnanta <raizal.travlr@gmail.com>
2020-03-20 22:53:52 +0800

    [FEATURE] Add Travlr-Icons (#1)

--- Source/SwiftIcons.swift
+++ Source/SwiftIcons.swift
@@ -754,7 +754,8 @@ private var loadedFontsTracker: [String: Bool] = ["dripicons-v2": false,
                                                   "open-iconic": false,
                                                   "StateFace-Regular": false,
                                                   "WeatherIcons-Regular": false,
-                                                  "typicons": false
+                                                  "typicons": false,
+                                                  "travlr_icons": false
 ]
 
 protocol FontProtocol {
@@ -826,6 +827,8 @@ public enum FontType: FontProtocol {
     /// It selects typIcons icons for the particular object from the library
     case typIcons(TypIconsType)
 
+    case travlrIcons(TravlrIconsType)
+    
     /**
      This function returns the font name using font type
      */
@@ -860,6 +863,8 @@ public enum FontType: FontProtocol {
             fontName = "WeatherIcons-Regular"
         case .typIcons:
             fontName = "typicons"
+        case .travlrIcons:
+            fontName = "travlr_icons"
         }
         return fontName
     }
@@ -898,6 +903,8 @@ public enum FontType: FontProtocol {
             fileName = "WeatherIcons"
         case .typIcons:
             fileName = "TypIcons"
+        case .travlrIcons:
+            fileName = "travlr_icons"
         }
         return fileName
     }
@@ -936,6 +943,8 @@ public enum FontType: FontProtocol {
             familyName = "Weather Icons"
         case .typIcons:
             familyName = "Typicons"
+        case .travlrIcons:
+            familyName = "travlr_icons"
         }
         return familyName
     }
@@ -984,6 +993,8 @@ public enum FontType: FontProtocol {
         case let .weather(icon):
             text = icon.text!
         case let .typIcons(icon):
+            text = icon.text!
+        case let .travlrIcons(icon):
             text = icon.text!
         }
 
@@ -1358,3 +1369,17 @@ public enum TypIconsType: Int {
 }
 
 private let typIcons = ["\u{e000}", "\u{e001}", "\u{e002}", "\u{e003}", "\u{e004}", "\u{e005}", "\u{e006}", "\u{e007}", "\u{e008}", "\u{e009}", "\u{e00a}", "\u{e00b}", "\u{e00c}", "\u{e00d}", "\u{e00e}", "\u{e00f}", "\u{e010}", "\u{e011}", "\u{e012}", "\u{e013}", "\u{e014}", "\u{e015}", "\u{e016}", "\u{e017}", "\u{e018}", "\u{e019}", "\u{e01a}", "\u{e01b}", "\u{e01c}", "\u{e01d}", "\u{e01e}", "\u{e01f}", "\u{e020}", "\u{e021}", "\u{e022}", "\u{e023}", "\u{e024}", "\u{e025}", "\u{e026}", "\u{e027}", "\u{e028}", "\u{e029}", "\u{e02a}", "\u{e02b}", "\u{e02c}", "\u{e02d}", "\u{e02e}", "\u{e02f}", "\u{e030}", "\u{e031}", "\u{e032}", "\u{e033}", "\u{e034}", "\u{e035}", "\u{e036}", "\u{e037}", "\u{e038}", "\u{e039}", "\u{e03a}", "\u{e03b}", "\u{e03c}", "\u{e03d}", "\u{e03e}", "\u{e03f}", "\u{e040}", "\u{e041}", "\u{e042}", "\u{e043}", "\u{e044}", "\u{e045}", "\u{e046}", "\u{e047}", "\u{e048}", "\u{e049}", "\u{e04a}", "\u{e04b}", "\u{e054}", "\u{e04c}", "\u{e04d}", "\u{e04e}", "\u{e04f}", "\u{e050}", "\u{e051}", "\u{e052}", "\u{e053}", "\u{e055}", "\u{e056}", "\u{e057}", "\u{e058}", "\u{e059}", "\u{e05a}", "\u{e05b}", "\u{e05c}", "\u{e05d}", "\u{e05e}", "\u{e05f}", "\u{e060}", "\u{e061}", "\u{e062}", "\u{e063}", "\u{e064}", "\u{e065}", "\u{e066}", "\u{e067}", "\u{e068}", "\u{e069}", "\u{e06a}", "\u{e06b}", "\u{e06c}", "\u{e06d}", "\u{e06e}", "\u{e06f}", "\u{e070}", "\u{e071}", "\u{e072}", "\u{e073}", "\u{e074}", "\u{e075}", "\u{e076}", "\u{e077}", "\u{e078}", "\u{e079}", "\u{e07a}", "\u{e07b}", "\u{e07c}", "\u{e07d}", "\u{e07e}", "\u{e07f}", "\u{e080}", "\u{e081}", "\u{e082}", "\u{e083}", "\u{e084}", "\u{e085}", "\u{e086}", "\u{e087}", "\u{e088}", "\u{e089}", "\u{e08a}", "\u{e08b}", "\u{e08c}", "\u{e08d}", "\u{e08e}", "\u{e08f}", "\u{e090}", "\u{e091}", "\u{e092}", "\u{e093}", "\u{e094}", "\u{e095}", "\u{e096}", "\u{e097}", "\u{e098}", "\u{e099}", "\u{e09a}", "\u{e09b}", "\u{e09c}", "\u{e09d}", "\u{e09e}", "\u{e09f}", "\u{e0a0}", "\u{e0a1}", "\u{e0a2}", "\u{e0a3}", "\u{e0a4}", "\u{e0a5}", "\u{e0a6}", "\u{e0a7}", "\u{e0a8}", "\u{e0a9}", "\u{e0aa}", "\u{e0ab}", "\u{e0ac}", "\u{e0ad}", "\u{e0ae}", "\u{e0af}", "\u{e0b0}", "\u{e0b1}", "\u{e0b2}", "\u{e0b3}", "\u{e0b4}", "\u{e0b5}", "\u{e0b6}", "\u{e0b7}", "\u{e0b8}", "\u{e0b9}", "\u{e0ba}", "\u{e0bb}", "\u{e0bc}", "\u{e0bd}", "\u{e0be}", "\u{e0bf}", "\u{e0c0}", "\u{e0c1}", "\u{e0c2}", "\u{e0c3}", "\u{e0c4}", "\u{e0c5}", "\u{e0c6}", "\u{e0c7}", "\u{e0c8}", "\u{e0c9}", "\u{e0ca}", "\u{e0cb}", "\u{e0cc}", "\u{e0cd}", "\u{e0ce}", "\u{e0cf}", "\u{e0d0}", "\u{e0d1}", "\u{e0d2}", "\u{e0d3}", "\u{e0d4}", "\u{e0d5}", "\u{e0d6}", "\u{e0d7}", "\u{e0d8}", "\u{e0d9}", "\u{e0da}", "\u{e0db}", "\u{e0dc}", "\u{e0dd}", "\u{e0de}", "\u{e0df}", "\u{e0e0}", "\u{e0e1}", "\u{e0e2}", "\u{e0e3}", "\u{e0e4}", "\u{e0e5}", "\u{e0e6}", "\u{e0e7}", "\u{e0e8}", "\u{e0e9}", "\u{e0ea}", "\u{e0eb}", "\u{e0ec}", "\u{e0ed}", "\u{e0ee}", "\u{e0ef}", "\u{e0f0}", "\u{e0f1}", "\u{e0f2}", "\u{e0f3}", "\u{e0f4}", "\u{e0f5}", "\u{e0f6}", "\u{e0f7}", "\u{e0f8}", "\u{e0f9}", "\u{e0fa}", "\u{e0fb}", "\u{e0fc}", "\u{e0fd}", "\u{e0fe}", "\u{e0ff}", "\u{e100}", "\u{e101}", "\u{e102}", "\u{e103}", "\u{e104}", "\u{e105}", "\u{e106}", "\u{e107}", "\u{e108}", "\u{e109}", "\u{e10a}", "\u{e10b}", "\u{e10c}", "\u{e10d}", "\u{e10e}", "\u{e10f}", "\u{e110}", "\u{e111}", "\u{e112}", "\u{e113}", "\u{e114}", "\u{e115}", "\u{e116}", "\u{e117}", "\u{e118}", "\u{e119}", "\u{e11a}", "\u{e11b}", "\u{e11c}", "\u{e11d}", "\u{e11e}", "\u{e11f}", "\u{e120}", "\u{e121}", "\u{e122}", "\u{e123}", "\u{e124}", "\u{e125}", "\u{e126}", "\u{e127}", "\u{e128}", "\u{e129}", "\u{e12a}", "\u{e12b}", "\u{e12c}", "\u{e12d}", "\u{e12e}", "\u{e12f}", "\u{e130}", "\u{e131}", "\u{e132}", "\u{e133}", "\u{e134}", "\u{e135}", "\u{e136}", "\u{e137}", "\u{e138}", "\u{e139}", "\u{e13a}", "\u{e13b}", "\u{e13c}", "\u{e13d}", "\u{e13e}", "\u{e13f}", "\u{e140}", "\u{e141}", "\u{e142}", "\u{e143}", "\u{e144}", "\u{e145}", "\u{e146}", "\u{e147}", "\u{e148}", "\u{e149}", "\u{e14a}", "\u{e14b}", "\u{e14c}", "\u{e14d}", "\u{e14e}", "\u{e14f}"]
+
+public enum TravlrIconsType: Int {
+    public static var count: Int {
+        return travlrIcons.count
+    }
+    
+    public var text: String? {
+        return travlrIcons[rawValue]
+    }
+    
+    case time24h, addList, aircon, angleDownSmall, angleDown, activity, angleLeft, angleRightSmall, angleLeftSmall, angleUpSmall, accessibility, arrowDownLeft, angleUp, angleRight, arrowDown, account, arrowRight, arrowUpLong, arrowDownRight, arrowUp, arrowsAlt, arrowUpRight, arrowsExpand, arrowLeft, arrowLeftLong, arrowRightLong, arrowUpLeft, arrowDownLong, arrowsCompress, arrows, articles, arrowsReverse, baby, bar, basket, attachment, arrowsV, bookmark, bubbleO, beach, building, bookmarkO, arrowsH, cadetDownCircle, bubble, cadetDownSquareO, bus, cadetDown, cadetDownSmall, cadetDownCircleO, cadetUpSmall, cadetUpSquareO, cadetUpSquare, cadetUp, cafe, cadetUpCircleO, calendarDel, calendarList, calendarMin, calendarPlus, calendar, callOff, call, cameraO, camera, cardAmex, cardMastercard, cardPaypal, cardVisa, card, cartO, cart, chart, checkCircleO, checkCircle, checkSmall, checkSquareO, checkSquare, check, calendarCheck, closeCircleO, closeCircle, closeSmall, closeSquareO, closeSquare, close, checkbox, cloudDown, cloudUpO, cloudUp, cloudDownO, commentO, cadetUpCircle, community, compress, conversationO, conversation, copy, deleteO, delete, direction, discover, dollar, download, editO, edit, ellipsisH, ellipsisV, errorO, error, events, expand, external, eyeOOff, eyeO, eyeOff, eye, facebookSquare, facebook, file, filter, fire, flagO, flag, flightArrival, flightDeparture, flight, food, forward, funnel, gift, globe, googlePlay, googlePlus, google, gpsNone, gpsOff, gps, grid, guarantee, gym, heartO, heart, helpO, help, homeO, home, hotel, imageLibraryO, imageLibrary, imageO, image, inbox, infoO, info, instagram, laundry, link, linkedinSquare, linkedin, listCircle, listSquare, list, location, lockedO, locked, login, logout, mailO, mailOpenO, mailOpen, mail, comment, map, menu, cadetDownSquare, message, code, minusCircleO, minusCircle, minusSmall, messageO, mapO, minus, navigation45, minusSquare, note, minusSquareO, notificationOffO, notificationOff, notification, navigation90, pinpoint, pinterestCircle, pinterest, places, plusCircleO, plusCircle, plusSmall, plusSquareO, plusSquare, plus, pool, priceTag, priceTags, print, receipt, recycle, refresh, renew, replyAll, reply, save, search, selected, send, setting, shareO, share, shieldO, shield, ship, shop, shopping, shortlists, sortDown, sortUp, spa, spinner1, spinner2, spinner3, starO, starRatingEmpty, starRatingFilled, starRatingHalf, star, stop, sun, sync, taxi, tbb, timeO, time, timerO, timer, tool, transport, travlr, trendDown, trendFlat, trendUp, tripAdd, trip, trophy, tv, twitter, unlockedO, unlocked, unselected, upload, userAddO, userAdd, userGroupO, userGroup, userO, user, voucher, wedding, whatsapp, wifi, youtube, zoomIn, zoomOut, messenger, park, notificationO
+}
+
+private let travlrIcons = ["\u{ea02}","\u{ea03}","\u{ea04}","\u{ea05}","\u{ea06}","\u{ea07}","\u{ea08}","\u{ea09}","\u{ea0a}","\u{ea0b}","\u{ea0c}","\u{ea0d}","\u{ea0e}","\u{ea0f}","\u{ea10}","\u{ea11}","\u{ea12}","\u{ea13}","\u{ea14}","\u{ea15}","\u{ea16}","\u{ea17}","\u{ea18}","\u{ea19}","\u{ea1a}","\u{ea1b}","\u{ea1c}","\u{ea1d}","\u{ea1e}","\u{ea1f}","\u{ea20}","\u{ea21}","\u{ea22}","\u{ea23}","\u{ea24}","\u{ea25}","\u{ea26}","\u{ea27}","\u{ea28}","\u{ea29}","\u{ea2a}","\u{ea2b}","\u{ea2c}","\u{ea2d}","\u{ea2e}","\u{ea2f}","\u{ea30}","\u{ea31}","\u{ea32}","\u{ea33}","\u{ea34}","\u{ea35}","\u{ea36}","\u{ea37}","\u{ea38}","\u{ea39}","\u{ea3a}","\u{ea3b}","\u{ea3c}","\u{ea3d}","\u{ea3e}","\u{ea3f}","\u{ea40}","\u{ea41}","\u{ea42}","\u{ea43}","\u{ea44}","\u{ea45}","\u{ea46}","\u{ea47}","\u{ea48}","\u{ea49}","\u{ea4a}","\u{ea4b}","\u{ea4c}","\u{ea4d}","\u{ea4e}","\u{ea4f}","\u{ea50}","\u{ea51}","\u{ea52}","\u{ea53}","\u{ea54}","\u{ea55}","\u{ea56}","\u{ea57}","\u{ea58}","\u{ea59}","\u{ea5a}","\u{ea5b}","\u{ea5c}","\u{ea5d}","\u{ea5e}","\u{ea5f}","\u{ea60}","\u{ea61}","\u{ea62}","\u{ea63}","\u{ea64}","\u{ea65}","\u{ea66}","\u{ea67}","\u{ea68}","\u{ea69}","\u{ea6a}","\u{ea6b}","\u{ea6c}","\u{ea6d}","\u{ea6e}","\u{ea6f}","\u{ea70}","\u{ea71}","\u{ea72}","\u{ea73}","\u{ea74}","\u{ea75}","\u{ea76}","\u{ea77}","\u{ea78}","\u{ea79}","\u{ea7a}","\u{ea7b}","\u{ea7c}","\u{ea7d}","\u{ea7e}","\u{ea7f}","\u{ea80}","\u{ea81}","\u{ea82}","\u{ea83}","\u{ea84}","\u{ea85}","\u{ea86}","\u{ea87}","\u{ea88}","\u{ea89}","\u{ea8a}","\u{ea8b}","\u{ea8c}","\u{ea8d}","\u{ea8e}","\u{ea8f}","\u{ea90}","\u{ea91}","\u{ea92}","\u{ea93}","\u{ea94}","\u{ea95}","\u{ea96}","\u{ea97}","\u{ea98}","\u{ea99}","\u{ea9a}","\u{ea9b}","\u{ea9c}","\u{ea9d}","\u{ea9e}","\u{ea9f}","\u{eaa0}","\u{eaa1}","\u{eaa2}","\u{eaa3}","\u{eaa4}","\u{eaa5}","\u{eaa6}","\u{eaa7}","\u{eaa8}","\u{eaa9}","\u{eaaa}","\u{eaab}","\u{eaac}","\u{eaad}","\u{eaae}","\u{eaaf}","\u{eab0}","\u{eab1}","\u{eab2}","\u{eab3}","\u{eab4}","\u{eab5}","\u{eab6}","\u{eab7}","\u{eab8}","\u{eab9}","\u{eaba}","\u{eabb}","\u{eabc}","\u{eabd}","\u{eabe}","\u{eabf}","\u{eac0}","\u{eac1}","\u{eac2}","\u{eac3}","\u{eac4}","\u{eac5}","\u{eac6}","\u{eac7}","\u{eac8}","\u{eac9}","\u{eaca}","\u{eacb}","\u{eacc}","\u{eacd}","\u{eace}","\u{eacf}","\u{ead0}","\u{ead1}","\u{ead2}","\u{ead3}","\u{ead4}","\u{ead5}","\u{ead6}","\u{ead7}","\u{ead8}","\u{ead9}","\u{eada}","\u{eadb}","\u{eadc}","\u{eadd}","\u{eade}","\u{eadf}","\u{eae0}","\u{eae1}","\u{eae2}","\u{eae3}","\u{eae4}","\u{eae5}","\u{eae6}","\u{eae7}","\u{eae8}","\u{eae9}","\u{eaea}","\u{eaeb}","\u{eaec}","\u{eaed}","\u{eaee}","\u{eaef}","\u{eaf0}","\u{eaf1}","\u{eaf2}","\u{eaf3}","\u{eaf4}","\u{eaf5}","\u{eaf6}","\u{eaf7}","\u{eaf8}","\u{eaf9}","\u{eafa}","\u{eafb}","\u{eafc}","\u{eafd}","\u{eafe}","\u{eaff}","\u{eb00}","\u{eb01}","\u{eb02}","\u{eb03}","\u{eb04}","\u{eb05}","\u{eb06}","\u{eb07}","\u{eb08}","\u{eb09}","\u{eb0a}","\u{eb0b}","\u{eb0c}","\u{eb0d}","\u{eb0e}","\u{eb0f}","\u{eb10}","\u{eb11}","\u{eb12}","\u{eb13}","\u{eb14}","\u{eb15}"]
